3df5e0730bed8e2657aa609d1fa243a7b1bcca8e
Daniel Santos <daniel.santos.p@uni.pe>
2021-03-28 17:19:20 -0500

    Primer commit del proyecto

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">CCAT Exchange v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Este es un proyecto para obtener las cotizaciones de la cryptomonedas más
+      importantes a traves de la API de CoinCap y VueJS
+      <br />
+      Este proyecto es utilizado como primer entregable para la página web del
+      CCAT UNI
+    </p>
+    <router-link to="/" class="mt-5 text-xl text-blue-600 hover:underline">
+      Volver a la página de Inicio
+    </router-link>
+  </div>
+</template>
